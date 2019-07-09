63d181d7cb457105f5a04bb2b799b7fd49bc206c
shenxf1986 <185693240@qq.com>
2019-07-09 09:52:34 +0800

    update

--- react-koa-typescript-ssr/src/client/server-entry.tsx
+++ react-koa-typescript-ssr/src/client/server-entry.tsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import { StaticRouter } from 'react-router-dom';
 import App from '../shared/containers/App';
+import { Provider, useStaticRenderidng } from 'mobx-react';
 
-export default <App />;
+// 让mobx在服务端渲染的时候不会重复数据变换
+useStaticRenderidng(true);
+
+export default (stores, routerContent, url, sheetsRegistry, generateClassName, theme) => (
+    <Provider {...stores} >
+        <StaticRouter context={routerContent} location={url}>
+            <App />
+        </StaticRouter>
+    </Provider>
+);
