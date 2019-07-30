ea92aeff0f0f7d3b1999b9b84fa8a531e8d1496a
shenxf1986 <185693240@qq.com>
2019-07-30 18:09:51 +0800

    delete

--- react-koa-typescript-ssr/src/serverWithNoTypeScript/util/request.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-const instance = axios.create({
-  baseURL: 'https://cnodejs.org',
-  timeout: 10000,
-  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-});
-
-module.exports = instance;
