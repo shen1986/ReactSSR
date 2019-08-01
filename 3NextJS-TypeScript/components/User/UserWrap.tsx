ea20c34d419a2926ea916229ce83e43cf90ca860
shenxf1986 <185693240@qq.com>
2019-08-01 10:57:54 +0800

    update

--- 3NextJS-TypeScript/components/User/UserWrap.tsx
+++ 3NextJS-TypeScript/components/User/UserWrap.tsx
@@ -1,39 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { PropTypes as propTypes } from 'mobx-react';
-import { withStyles } from '@material-ui/core/styles';
+import {
+    inject,
+    observer,
+} from 'mobx-react';
+
 import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from '@material-ui/styles';
+
 import UserIcon from '@material-ui/icons/AccountCircle';
-import ContentWrap from '../Common/ContentWrap';
-import userWrapStyle from './styles/userWrapStyle';
 
-const UserWrap = ({ user, children, classes }) => (
-  <ContentWrap>
-    <div className={classes.avatar}>
-      <div className={classes.bg} />
-      {
-        user.isLogin
-          ? <Avatar className={classes.avatarImg} src={user.info.avatar_url} />
-          : (
-            <Avatar className={classes.avatarImg}>
-              <UserIcon />
-            </Avatar>
-          )
-      }
-      <span className={classes.userName}>
-        {
-          user.info.loginName || '未登录'
-        }
-      </span>
-    </div>
-    { children }
-  </ContentWrap>
-);
+import Container from '../Common/ContentWrap';
+import userStyles from './styles/user-style';
 
-UserWrap.propTypes = {
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]),
-  user: propTypes.observableObject,
-  classes: PropTypes.object,
+@inject((stores: any) => {
+    return {
+        user: stores.userStore.user,
+    };
+}) @observer
+class User extends React.Component<any> {
+    componentDidMount() {
+        // do someting here
+    }
+
+    render() {
+        const classes = this.props.classes;
+        const user = this.props.user.info || {};
+        return (
+            <Container>
+                <div>
+                    <div className={classes.avatar}>
+                        <div className={classes.bg} />
+                        {
+                            user.avatar_url ?
+                                <Avatar className={classes.avatarImg} src={user.avatar_url} /> :
+                                <Avatar className={classes.avatarImg}>
+                                    <UserIcon />
+                                </Avatar>
+                        }
+                        <span className={classes.userName}>{user.loginName || '未登录'}</span>
+                    </div>
+                    {this.props.children}
+                </div>
+            </Container>
+        );
+    }
+}
+
+(User as any).wrappedComponent.propTypes = {
+    user: PropTypes.object.isRequired,
 };
 
-export default withStyles(userWrapStyle as any)(UserWrap);
+(User as any).propTypes = {
+    classes: PropTypes.object.isRequired,
+    children: PropTypes.element.isRequired,
+};
+
+export default withStyles(userStyles as any)(User);
--- 3NextJS-TypeScript/pages/login.tsx
+++ 3NextJS-TypeScript/pages/login.tsx
@@ -1,33 +1,110 @@
-import React, { Component } from 'react';
-import { observer, inject, PropTypes as propTypes } from 'mobx-react';
-// import PropTypes from 'prop-types';
-import UserWrap from '../components/User/UserWrap';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+    inject,
+    observer,
+} from 'mobx-react';
+import queryString from 'querystring';
 
-interface IProps {
-    user: any;
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/styles';
+import { withRouter } from 'next/router';
+
+import UserWrapper from '../components/User/UserWrap';
+import loginStyles from '../components/User/styles/loginStyle';
+
+class UserLogin extends React.Component<any, any> {
+    static contextTypes = {
+        router: PropTypes.object,
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            accesstoken: '',
+            helpText: '',
+        };
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    getFrom(location?: any) {
+        console.log(this.props.router);
+        const myLocation = location || this.props.router.pathname;
+        console.log(this.props.router.pathname);
+        const query = queryString.parse(myLocation.search);
+        return query.from || '/user/info';
+    }
+
+    handleLogin() {
+        // handle login here
+        if (!this.state.accesstoken) {
+            return this.setState({
+                helpText: '必须填写',
+            });
+        }
+        this.setState({
+            helpText: '',
+        });
+        return this.props.appState.login(this.state.accesstoken)
+            .catch((msg) => {
+                this.props.appState.notify({ message: msg });
+            });
+    }
+
+    handleInput(event) {
+        this.setState({
+            accesstoken: event.target.value.trim(),
+        });
+    }
+
+    render() {
+        const classes = this.props.classes;
+        const isLogin = this.props.user.isLogin;
+        const from = this.getFrom();
+
+        if (isLogin) {
+            return this.props.router.push(from as string);
+                // <Redirect to={from} />
+        }
+
+        return (
+            <UserWrapper>
+                <div className={classes.root}>
+                    <TextField
+                        label="请输入Cnode AccessToken"
+                        placeholder="请输入Cnode AccessToken"
+                        required
+                        helperText={this.state.helpText}
+                        value={this.state.accesstoken}
+                        onChange={this.handleInput}
+                        className={classes.input}
+                    />
+                    <Button
+                        variant="outlined"
+                        color="secondary"
+                        onClick={this.handleLogin}
+                        className={classes.loginButton}
+                    >
+                        登 录
+                    </Button>
+                </div>
+            </UserWrapper>
+        );
+    }
 }
 
-@inject((stores: any) => ({
-  user: stores.userStore.user,
-  login: stores.userStore.login,
-})) @observer
-class Login extends Component<IProps> {
-  state = {};
-
-  render () {
-    return (
-      <UserWrap user={this.props.user}>
-        <div>
-          login
-        </div>
-      </UserWrap>
-    );
-  }
-}
-
-(Login as any).propTypes = {
-  // login: PropTypes.func,
-  user: propTypes.observableObject,
+(UserLogin as any).propTypes = {
+    classes: PropTypes.object.isRequired,
+    appState: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
 };
 
-export default Login;
+export default withRouter(withStyles(loginStyles as any)(inject((stores: any) => {
+    return {
+        appState: stores.appState,
+        user: stores.userStore.user,
+    };
+})(observer(UserLogin))) as any);
